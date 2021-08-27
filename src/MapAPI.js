33014dbfdc763511700f54739a723cd3d53314c5
Atlegang Molokwane <amolokwane7@gmail.com>
2021-08-27 10:38:02 +0200

    Cleaned code a bit

--- src/MapAPI.js
+++ src/MapAPI.js
@@ -1,67 +1,103 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
 import { LoadScript } from "@react-google-maps/api";
 import Maps from "./Maps";
 import "./MapAPI.css";
 
-
 class MapAPI extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            apiRes: [],
-        };
-        
-    }
-    apiUrl =() =>{
-        const config = {
-                method: 'get',
-                url: 'https://sherlock.aerobotics.com/developers/orchards/',
-                headers: {
-                  'accept': 'application/json',
-                  'Authorization': '1566394169B0EJX2MGAVKVUGGKEMKZBMND9A7VCR',
-                  'X-CSRFToken': 'lhpuvU3EOjWK6PfrllaoBpAd0CaXBF1o3fX5RmZoduIWYZl5uMXKmI2hHldlYTOb'
-                },
-            }
-          
-              axios(config)
-              .then(function (response) {
-                console.log(JSON.stringify(response.data.results[0]));
-                var res = JSON.stringify(response.data.results[0]);
-                return res
-              }).then(res => {
-                      console.log('This is your data', res)
-                      if(res){
-                          this.setState({
-                              apiRes: [...this.state.apiRes, ...res]
-                          })
-                      }
-                    })
-              .catch(function (error) {
-                console.log(error);
-              });
-              
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiRes: [],
+      treeRes: [],
+    };
+  }
+  apiUrl = () => {
+    const config = {
+      method: "get",
+      url: "https://sherlock.aerobotics.com/developers/orchards/",
+      headers: {
+        accept: "application/json",
+        Authorization: "1566394169B0EJX2MGAVKVUGGKEMKZBMND9A7VCR",
+        "X-CSRFToken":
+          "lhpuvU3EOjWK6PfrllaoBpAd0CaXBF1o3fX5RmZoduIWYZl5uMXKmI2hHldlYTOb",
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        var res = JSON.stringify(response.data.results[0]);
+        var res1 = JSON.parse(res);
+        return (res1 = [res1].flat());
+      })
+      .then((res) => {
+        if (res) {
+          this.setState({
+            apiRes: [...this.state.apiRes, ...res],
+          });
         }
-    componentDidMount(){
-        this.apiUrl();  
-    }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  treeUrl = () => {
+    const config = {
+      method: "get",
+      url: "https://sherlock.aerobotics.com/developers/treesurveys/",
+      headers: {
+        accept: "application/json",
+        Authorization: "1566394169B0EJX2MGAVKVUGGKEMKZBMND9A7VCR",
+        "X-CSRFToken":
+          "6f83nAwjINIVgw7kWRiN2lSf2VDf00YvOdGEJ2s37Yu78GdY5i59NEkjJEGDneLi",
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        var res = JSON.stringify(response.data);
+        var res1 = JSON.parse(res);
+        return (res1 = [res1].flat());
+      })
+      .then((res) => {
+        if (res) {
+          this.setState({
+            treeRes: [...this.state.treeRes, ...res],
+          });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  componentDidMount() {
+    this.treeUrl();
+    this.apiUrl();
+  }
 
   render() {
-      
     return (
-    <div >
-        <h2 className = "head_border">Farm Info</h2>
-        <div className = "p_block">           
-            <p className = "border">Client_id : 10479</p>
-            <p className = "border">Name : 03 Olinda valencia </p>
-            <p className = "border">Farm_id : 8919</p>
-            <p className = "border">Hectares : 2.2419824367885</p>           
+      <div>
+        <h2 className="head_border">Farm Info</h2>
+        <div>
+          {this.state.apiRes.map((data) => {
+            return (
+              <div className="p_block" key={data.id}>
+                <p className="border">Client_id : {data.client_id}</p>
+                <p className="border">Name : {data.name} </p>
+                <p className="border">Farm_id : {data.farm_id}</p>
+                <p className="border">Hectares : {data.hectares}</p>
+              </div>
+            );
+          })}
         </div>
         <LoadScript googleMapsApiKey="AIzaSyDOpRsyAUVQtW6BdTlr6O7WzkLNDL_VIDY">
-            <Maps data= {this.state.apiRes} />
+          <Maps data={this.state.treeRes} />
         </LoadScript>
-    </div>
-  );}
+      </div>
+    );
+  }
 }
 
 export default MapAPI;
--- src/Maps.js
+++ src/Maps.js
@@ -1,108 +1,67 @@
 import React, { useState } from "react";
 import { GoogleMap, InfoWindow, Marker } from "@react-google-maps/api";
-import treeData from "./JSON/response_tree_surveys.json";
-
-
-// const markers = [
-//   {
-//     id: 1,
-//     name: "Chicago, Illinois",
-//     position: { lat: 41.881832, lng: -87.623177 }
-//   },
-//   {
-//     id: 2,
-//     name: "Denver, Colorado",
-//     position: { lat: 39.739235, lng: -104.99025 }
-//   },
-//   {
-//     id: 3,
-//     name: "Los Angeles, California",
-//     position: { lat: 34.052235, lng: -118.243683 }
-//   },
-//   {
-//     id: 4,
-//     name: "New York, New York",
-//     position: { lat: 40.712776, lng: -74.005974 }
-//   }
-// ];
 
 function Maps(props) {
   const [activeMarker, setActiveMarker] = useState(null);
+  if (props.data.length === 0) {
+    return <h2> Loading....</h2>;
+  } else {
+    const handleActiveMarker = (marker) => {
+      if (marker === activeMarker) {
+        return;
+      }
+      setActiveMarker(marker);
+    };
 
-  const handleActiveMarker = (marker) => {
-    if (marker === activeMarker) {
-      return;
-    }
-    setActiveMarker(marker);
-  };
-
-//   const handleOnLoad = (map) => {
-//     const bounds = new window.google.maps.LatLngBounds();
-//     markers.forEach(({ position }) => bounds.extend(position));
-//     map.fitBounds(bounds);
-//   };
-  const handleOnLoad = (map) => 
-    treeData.map((data) =>{
-      return(
-      <div>
-          {data.results.map((data) =>{
-              let position = {lat: data.latitude, lng: data.longitude}
+    let treesData = props.data;
+    const handleOnLoad = (map) => {
+        treesData.map((data) => {
+        return (
+          <div>
+            {data.results.map((data) => {
+              let position = { lat: data.latitude, lng: data.longitude };
               const bounds = new window.google.maps.LatLngBounds();
               bounds.extend(position);
-              return (map.fitBounds(bounds));
+              return map.fitBounds(bounds);
             })}
-      </div>
-      )
-  });
+          </div>
+        );
+      });
+    };
 
-  return (
-    <div>
-    <GoogleMap
-      onLoad={handleOnLoad}
-      onClick={() => setActiveMarker(null)}
-      mapContainerStyle={{ width: "100%", height: "700px" }}
-    >
-    {
-        treeData.map((data) =>{
-            return(
-            <div>
-                {data.results.map((data) =>{
-                    let position = {lat: data.latitude, lng: data.longitude};
-                    return(
-                    <Marker
-                    key={data.id}
-                    position={position}
-                    onClick={() => handleActiveMarker(data.id)}
-                  >
-                    {activeMarker === data.id ? (
-                      <InfoWindow onCloseClick={() => setActiveMarker(null)}>
-                        <div>{data.tree_id}</div>
-                      </InfoWindow>
-                    ) : null}
-                  </Marker>
-                    )
-                  })}
-            </div>
-            )
-        })
-    }
-      {/* {
-      markers.map(({ id, name, position }) => (
-        <Marker
-          key={id}
-          position={position}
-          onClick={() => handleActiveMarker(id)}
+    return (
+      <div>
+        <GoogleMap
+          onLoad={handleOnLoad}
+          onClick={() => setActiveMarker(null)}
+          mapContainerStyle={{ width: "100%", height: "700px" }}
         >
-          {activeMarker === id ? (
-            <InfoWindow onCloseClick={() => setActiveMarker(null)}>
-              <div>{name}</div>
-            </InfoWindow>
-          ) : null}
-        </Marker>
-      ))} */}
-    </GoogleMap>
-    </div>
-  );
+          {treesData.map((data) => {
+            return (
+              <div>
+                {data.results.map((data) => {
+                  let position = { lat: data.latitude, lng: data.longitude };
+                  return (
+                    <Marker
+                      key={data.id}
+                      position={position}
+                      onClick={() => handleActiveMarker(data.id)}
+                    >
+                      {activeMarker === data.id ? (
+                        <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                          <div>{data.tree_id}</div>
+                        </InfoWindow>
+                      ) : null}
+                    </Marker>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </GoogleMap>
+      </div>
+    );
+  }
 }
 
 export default Maps;
